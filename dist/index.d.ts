b85c18be49a1211b050df07ef98ec8d1fdb52ca5
Jon Laing <jon.dreaux.laing@gmail.com>
2022-05-13 03:04:32 -0400

    forgot to export everthing

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,4 @@
-export {};
+export * as base from "./base";
+export * as control from "./control";
+export * as data from "./data";
+export * as extensions from "./extensions";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as base from "./base";
-import * as control from "./control";
-import * as data from "./data";
-import * as extensions from "./extensions";
+export * as base from "./base";
+export * as control from "./control";
+export * as data from "./data";
+export * as extensions from "./extensions";
